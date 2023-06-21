1c35dd93ab67c3f0c050c00e4a6b15d75bd422d2
batmatt <01143867@pw.edu.pl>
2023-06-21 17:18:39 +0200

    move longitude and latitude to request route

--- main.py
+++ main.py
@@ -145,10 +145,8 @@ def add_post():
 def read_post(post_id: str):
     return data_gateway.read_post(post_id)
 
-@app.route('/posts/nearby', methods=['GET'])
-def get_posts_nearby():
-    longitude = float(request.args.get('longitude'))
-    latitude = float(request.args.get('latitude'))
+@app.route('/posts/<float:longitude>/<float:latitude>', methods=['GET'])
+def get_posts_nearby(longitude: float, latitude: float):
     radius = 100
 
     return data_gateway.get_posts_within_radius(longitude, latitude, radius)
